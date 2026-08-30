61fb6fb25ad2f13ac78d9489d41fb907bc2aea8e
Z User <z@container>
2026-08-30 02:21:08 +0000

    a01a48fd-0e98-42b9-8687-66240a10fcdd

--- .next/dev/server/server-reference-manifest.js
+++ .next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"J75GNYNf5nCAzhWP9JKAE7FDu+DpuogOHxSjn2cxKpc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
